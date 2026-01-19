239fef107e011b2f4d5d26c1cbb8165ec394110e
orhigani1 <orhigani1@gmail.com>
2026-01-19 08:40:23 +0200

    Update section_a.py

--- Question1/section_a.py
+++ Question1/section_a.py
@@ -1,17 +1,16 @@
 class Invoice:
     def __init__(self, invoice_id, customer_name, amount, tax_rate):
-        self.invoice_id = invoice_id              # public
-        self.customer_name = customer_name        # public
-        self._amount = amount                     # protected
-        self.tax_rate = tax_rate                  # public
-        self.__is_paid = False                    # private
+        self.invoice_id = invoice_id              
+        self.customer_name = customer_name        
+        self._amount = amount                     
+        self.tax_rate = tax_rate                  
+        self.__is_paid = False                    
 
-    # PROPERTY לקריאה של amount
+    
     @property
     def amount(self):
         return self._amount
 
-    # PROPERTY לכתיבה עם ולידציה
     @amount.setter
     def amount(self, new_amount):
         if new_amount > 0:
@@ -19,15 +18,16 @@ class Invoice:
         else:
             raise ValueError("Amount must be positive")
 
-    # חישוב סכום כולל
+
     def calculate_total(self):
         return self._amount * (1 + self.tax_rate)
 
-    # שינוי סטטוס תשלום
+    
     def mark_as_paid(self):
         self.__is_paid = True
 
-    # PROPERTY לקריאה בלבד של סטטוס תשלום
+    
     @property
     def is_paid(self):
         return self.__is_paid
+
